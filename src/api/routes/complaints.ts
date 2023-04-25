914b34968b4caf5fa336c59b02deb969b8f517c8
Gin Derick <ginderick@pop-os.localdomain>
2023-04-25 09:15:11 +0800

    update complaints route to inject complaints service

--- src/api/routes/complaints.ts
+++ src/api/routes/complaints.ts
@@ -1,4 +1,6 @@
+import ComplaintsService from '../../services/complaints';
 import {Router, Request, Response, NextFunction} from 'express';
+import Container from 'typedi';
 
 const route = Router();
 
@@ -6,7 +8,13 @@ const complaints = (app: Router) => {
   app.use('/complaints', route);
 
   route.get('/', async (req: Request, res: Response, next: NextFunction) => {
-    return res.status(201).json({Hello: 'world'});
+    try {
+      const complaintsService = Container.get(ComplaintsService);
+      const complaints = await complaintsService.getComplaints();
+      return res.status(200).json(complaints);
+    } catch (error) {
+      return next(error);
+    }
   });
 };
 
