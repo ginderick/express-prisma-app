7de3754fe72262b16ca7c66d4ca4afabc8ce12f7
Gin Derick <ginderick@pop-os.localdomain>
2023-04-25 21:36:30 +0800

    add logger in getComplaints

--- src/services/complaints.ts
+++ src/services/complaints.ts
@@ -1,10 +1,12 @@
-import {Service} from 'typedi';
+import {Inject, Service} from 'typedi';
+import {Logger} from 'winston';
 
 @Service()
 export default class ComplaintsService {
-  constructor() {}
+  constructor(@Inject('logger') private logger: Logger) {}
 
   public async getComplaints() {
+    this.logger.info('Service: Calling get complaints');
     return {Hello: 'World'};
   }
 }
