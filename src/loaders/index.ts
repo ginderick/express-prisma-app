5ae06046375b49e7e087635d117c1b37f0c1a2f4
Gin Derick <ginderick@pop-os.localdomain>
2023-04-25 18:34:24 +0800

    add dependencyInjector

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,5 +1,7 @@
+import dependencyInjector from './dependencyInjector';
 import expressLoader from './express';
 
 export default async ({expressApp}) => {
   expressLoader({app: expressApp});
+  dependencyInjector();
 };
