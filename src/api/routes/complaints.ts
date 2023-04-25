d309d27d04288782b19acabb140fbba36438e16a
Gin Derick <ginderick@pop-os.localdomain>
2023-04-25 22:17:13 +0800

    add post method for complaints endpoint to create a complaint

--- src/api/routes/complaints.ts
+++ src/api/routes/complaints.ts
@@ -9,12 +9,20 @@ const complaints = (app: Router) => {
   app.use('/complaints', route);
 
   route.get('/', async (req: Request, res: Response, next: NextFunction) => {
-    const logger: Logger = Container.get('logger');
-    logger.info('Complaints');
     try {
       const complaintsService = Container.get(ComplaintsService);
       const complaints = await complaintsService.getComplaints();
       return res.status(200).json(complaints);
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  route.post('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const complaintsService = Container.get(ComplaintsService);
+      const complaint = await complaintsService.createComplaint();
+      return res.status(201).json(complaint);
     } catch (error) {
       return next(error);
     }
