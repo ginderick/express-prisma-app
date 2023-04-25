3e43084fdc6c4e4707f3366eb0420c2103c63b82
Gin Derick <ginderick@pop-os.localdomain>
2023-04-25 22:15:56 +0800

    update complaints service to add getComplaints and createComplaint service

--- src/services/complaints.ts
+++ src/services/complaints.ts
@@ -1,12 +1,23 @@
-import {Inject, Service} from 'typedi';
+import ComplaintsRepository from '../repository/complaints';
+import Container, {Inject, Service} from 'typedi';
 import {Logger} from 'winston';
 
 @Service()
 export default class ComplaintsService {
-  constructor(@Inject('logger') private logger: Logger) {}
+  constructor(
+    @Inject('logger') private logger: Logger,
+    private complaintsRepository = Container.get(ComplaintsRepository)
+  ) {}
 
   public async getComplaints() {
-    this.logger.info('Service: Calling get complaints');
-    return {Hello: 'World'};
+    this.logger.info('Service: Getting complaints');
+    const complaints = this.complaintsRepository.getComplaints();
+    return complaints;
+  }
+
+  public async createComplaint() {
+    this.logger.info('Service: Creating a complaint');
+    const complaint = this.complaintsRepository.createComplaint();
+    return complaint;
   }
 }
