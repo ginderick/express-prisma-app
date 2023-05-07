f0f5e8431755b7ddef875f0de0e8bae534bde523
Gin Derick <ginderick@pop-os.localdomain>
2023-05-07 22:48:51 +0800

    add test for productsService

---
+++ tests/services/products.service.test.ts
@@ -0,0 +1,30 @@
+import {prismaMock} from '../singleton';
+import ProductsService from '../../src/services/products';
+import ProductsRepository from '../../src/repository/products';
+import loggerInstance from '../../src/loaders/logger';
+
+test('should get all products', async () => {
+  const product = {
+    id: 1,
+    sku: '123',
+    name: 'shoes',
+    quantity: 123,
+    price: 123,
+  };
+  const products = [product];
+
+  prismaMock.product.findMany.mockResolvedValue(products);
+  const logger = loggerInstance;
+  const productsRepository = new ProductsRepository(logger);
+  const productsService = new ProductsService(logger, productsRepository);
+
+  await expect(productsService.getProducts()).resolves.toEqual([
+    {
+      id: 1,
+      sku: '123',
+      name: 'shoes',
+      quantity: 123,
+      price: 123,
+    },
+  ]);
+});
