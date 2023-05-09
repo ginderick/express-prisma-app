be6bf357f5bbb2e6b10ac86478ad43b12f69e157
Gin Derick <ginderick@pop-os.localdomain>
2023-05-09 18:33:41 +0800

    move tests to 'unit' folder

--- tests/unit/routes/products.test.ts
+++ tests/unit/routes/products.test.ts
@@ -17,3 +17,14 @@ test('should return 200 for getting all products', async () => {
   const res = await request(server).get('/products');
   expect(res.status).toBe(200);
 });
+
+test('should return 201 when adding a product', async () => {
+  const payload = {
+    sku: '12345678',
+    name: 'shirt',
+    quantity: 2,
+    price: 132,
+  };
+  const res = await request(server).post('/products').send(payload);
+  expect(res.status).toBe(201);
+});
