231a642bedd70581647e43d9e036bbcf65720029
Gin Derick <ginderick@pop-os.localdomain>
2023-05-09 19:09:42 +0800

    add test for getProduct

--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -28,3 +28,20 @@ test('should get all products', async () => {
     },
   ]);
 });
+
+test('should get product using sku', async () => {
+  const product = {
+    id: 1,
+    sku: '123',
+    name: 'shoes',
+    quantity: 123,
+    price: 123,
+  };
+
+  prismaMock.product.findFirst.mockResolvedValue(product);
+  const logger = loggerInstance;
+  const productsRepository = new ProductsRepository(logger);
+  const productsService = new ProductsService(logger, productsRepository);
+
+  await expect(productsService.getProduct('123')).resolves.toEqual(product);
+});
