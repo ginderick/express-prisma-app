abe5c4a6f8458bb859133ee3514539e1ebf40b69
Gin Derick <ginderick@pop-os.localdomain>
2023-05-09 18:55:35 +0800

    add param schema for products

--- src/schema/ProductSchema.ts
+++ src/schema/ProductSchema.ts
@@ -7,4 +7,9 @@ export const ProductSchema = z.object({
   price: z.number(),
 });
 
+export const ProductParamSchema = z.object({
+  sku: z.string(),
+});
+
 export type ProductSchema = z.infer<typeof ProductSchema>;
+export type ProductParamSchema = z.infer<typeof ProductParamSchema>;
