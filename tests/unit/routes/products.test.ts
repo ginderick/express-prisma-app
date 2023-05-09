b059e6778a0458a4671e546cd3e0d25a0cbfcb30
Gin Derick <ginderick@pop-os.localdomain>
2023-05-09 20:06:46 +0800

    add tests for product not found and invalid product

--- tests/unit/routes/products.test.ts
+++ tests/unit/routes/products.test.ts
@@ -23,6 +23,11 @@ test('should return 200 for getting a product with sku', async () => {
   expect(res.status).toBe(200);
 });
 
+test('should return 404 for if product is not found', async () => {
+  const res = await request(server).get('/products/11111');
+  expect(res.status).toBe(404);
+});
+
 test('should return 201 when adding a product', async () => {
   const payload = {
     sku: '12345678',
@@ -32,4 +37,15 @@ test('should return 201 when adding a product', async () => {
   };
   const res = await request(server).post('/products').send(payload);
   expect(res.status).toBe(201);
+});
+
+test('should return 422 when adding an invalid product', async () => {
+  const payload = {
+    sku: '12345678',
+    name: 'shirt',
+    quantity: '2',
+    price: '132',
+  };
+  const res = await request(server).post('/products').send(payload);
+  expect(res.status).toBe(422);
 });
