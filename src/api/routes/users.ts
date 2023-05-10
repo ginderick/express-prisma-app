7cb710c059c536c8225fad9970c72533b5b9bd56
Gin Derick <ginderick@pop-os.localdomain>
2023-05-10 22:01:30 +0800

    add request validator middleware for /login route

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -3,7 +3,7 @@ import UsersService from '../../services/users';
 import {NextFunction, Request, Response, Router} from 'express';
 import Container from 'typedi';
 import middlewares from '../middlewares';
-import {UserSchema} from '../../schema/UserSchema';
+import {LoginUserSchema, UserSchema} from '../../schema/UserSchema';
 
 const route = Router();
 
@@ -26,23 +26,27 @@ const users = (app: Router) => {
     }
   );
 
-  route.post('/login', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const tokenService = Container.get(TokenService);
-      const usersService = Container.get(UsersService);
+  route.post(
+    '/login',
+    middlewares.validateRequest({body: LoginUserSchema}),
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const tokenService = Container.get(TokenService);
+        const usersService = Container.get(UsersService);
 
-      const username = req.body.username;
-      const password = req.body.password;
+        const username = req.body.username;
+        const password = req.body.password;
 
-      const user = await usersService.getUser(username);
-      if (!user) return res.status(401).send({error: 'Unauthorized'});
-      const token = await tokenService.generateAccessToken(username);
-      const refreshToken = await tokenService.generateAccessToken(username);
-      return res.status(200).json({access_token: token, refresh_token: refreshToken});
-    } catch (error) {
-      return next(error);
+        const user = await usersService.getUser(username);
+        if (!user) return res.status(401).send({error: 'Unauthorized'});
+        const token = await tokenService.generateAccessToken(username);
+        const refreshToken = await tokenService.generateAccessToken(username);
+        return res.status(200).json({access_token: token, refresh_token: refreshToken});
+      } catch (error) {
+        return next(error);
+      }
     }
-  });
+  );
 };
 
 export default users;
