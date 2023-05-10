f08dbfcee54cc53c6adf34f7c99f575dc1f3e637
Gin Derick <ginderick@pop-os.localdomain>
2023-05-10 18:59:01 +0800

    include refresh token in login endpoint

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -11,7 +11,8 @@ const users = (app: Router) => {
     try {
       const tokenService = Container.get(TokenService);
       const token = await tokenService.generateAccessToken('ginderick');
-      return res.status(200).json({access_token: token});
+      const refreshToken = await tokenService.generateAccessToken('ginderick');
+      return res.status(200).json({access_token: token, refresh_token: refreshToken});
     } catch (error) {
       return next(error);
     }
