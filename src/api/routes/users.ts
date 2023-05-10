fd689ada70a6c954ec28ef7b67e8883f70e122f1
Gin Derick <ginderick@pop-os.localdomain>
2023-05-10 18:54:36 +0800

    use token service to generate access token

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -1,4 +1,6 @@
+import TokenService from '../../services/token';
 import {NextFunction, Request, Response, Router} from 'express';
+import Container from 'typedi';
 
 const route = Router();
 
@@ -7,8 +9,9 @@ const users = (app: Router) => {
 
   route.post('/login', async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const test = {hello: 'World'};
-      return res.status(200).json(test);
+      const tokenService = Container.get(TokenService);
+      const token = await tokenService.generateAccessToken('ginderick');
+      return res.status(200).json({access_token: token});
     } catch (error) {
       return next(error);
     }
