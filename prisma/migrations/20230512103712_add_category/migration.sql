de328b0cdd845389dabfa199340f61dd4cf263d3
Gin Derick <ginderick@pop-os.localdomain>
2023-05-12 18:48:06 +0800

    add category in Products model

---
+++ prisma/migrations/20230512103712_add_category/migration.sql
@@ -0,0 +1,12 @@
+/*
+ Warnings:
+ - Added the required column `category` to the `Product` table without a default value. This is not possible if the table is not empty.
+ */
+
+-- AlterTable
+
+ALTER TABLE "Product"
+ADD
+    COLUMN "category" TEXT NOT NULL DEFAULT 'category';
+
+ALTER TABLE "Product" ALTER COLUMN "category" DROP DEFAULT;
