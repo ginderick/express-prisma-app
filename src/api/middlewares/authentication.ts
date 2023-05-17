090a55dda11c7ae7d1e908729883faa8b5320607
Gin <ginderick@gmail.com>
2023-05-17 19:48:45 +0800

    remove tokenPayload in authenticate middleware

--- src/api/middlewares/authentication.ts
+++ src/api/middlewares/authentication.ts
@@ -6,11 +6,10 @@ interface AuthenticatedRequest extends Request {
 }
 
 export function authenticate() {
-  return async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     try {
       const token = req.headers.authorization!.split(' ')[1];
       const decodedToken = jwt.verify(token, 'secretkey');
-      req.tokenPayload = decodedToken;
       next();
     } catch (error) {
       return res.status(401).json({message: 'Authentication failed'});
