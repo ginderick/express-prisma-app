6ebdbb1f258e958e969acd4e7b947e1b039cf93a
Gin <ginderick@gmail.com>
2023-05-17 20:09:45 +0800

    update jwt verify method to use privateJWTKey

--- src/api/middlewares/authentication.ts
+++ src/api/middlewares/authentication.ts
@@ -1,3 +1,4 @@
+import config from '../../config';
 import {NextFunction, Request, Response} from 'express';
 import jwt from 'jsonwebtoken';
 
@@ -9,7 +10,7 @@ export function authenticate() {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       const token = req.headers.authorization!.split(' ')[1];
-      const decodedToken = jwt.verify(token, 'secretkey');
+      jwt.verify(token, config.token.privateJWTKey!);
       next();
     } catch (error) {
       return res.status(401).json({message: 'Authentication failed'});
