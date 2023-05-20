6abc6c34578ab77c35d68e70d33f2d9202213f5a
Gin <ginderick@gmail.com>
2023-05-20 11:35:02 +0800

    initialize passport

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import routes from '../api';
+import passport from 'passport';
 
 const expressLoader = ({app}: {app: express.Application}) => {
   app.use(express.json());
+  app.use(passport.initialize());
   app.use('/', routes());
 };
 
