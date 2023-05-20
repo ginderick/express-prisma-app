ae1dd64bed78751541201444f9caaab20c5fe259
Gin <ginderick@gmail.com>
2023-05-20 10:45:44 +0800

    add authenticateMultiple middleware to be used for multiple login strategies

--- src/api/middlewares/authentication.ts
+++ src/api/middlewares/authentication.ts
@@ -1,6 +1,10 @@
 import config from '../../config';
 import {NextFunction, Request, Response} from 'express';
 import jwt from 'jsonwebtoken';
+import passport from 'passport';
+import {localStrategy} from '../../strategies/localStrategy';
+
+passport.use('local', localStrategy);
 
 export function authenticate() {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -13,3 +17,21 @@ export function authenticate() {
     }
   };
 }
+
+export const authenticateMultiple = (strategies: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate(strategies, (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (!user) {
+        return res.status(401).json({error: info || 'Authentication failed'});
+      }
+
+      const {hashed_password, email, ...userInfo} = user;
+      req.user = userInfo;
+      return next();
+    })(req, res, next);
+  };
+};
