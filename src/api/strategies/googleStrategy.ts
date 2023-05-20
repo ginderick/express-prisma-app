118af65e75c667ffc35584b1102156282bd1e615
Gin <ginderick@gmail.com>
2023-05-20 20:13:21 +0800

    remove console log

--- src/api/strategies/googleStrategy.ts
+++ src/api/strategies/googleStrategy.ts
@@ -5,22 +5,14 @@ export const googleStrategy = new GoogleStrategy(
   {
     clientID: config.google.clientId!,
     clientSecret: config.google.clientSecret!,
-    callbackURL: '/auth/google/callback', // Replace with your redirect URL
+    callbackURL: 'http:localhost:8001/auth/google/callback',
     scope: ['profile', 'email'],
   },
   (accessToken, refreshToken, profile, done) => {
-    // Extract the user information from the profile object
-    console.log('Access Token:', accessToken);
     const user = {
-      id: profile.id,
-      name: profile.displayName,
-      email: profile!.emails![0].value,
+      user: profile!.emails![0].value,
     };
-    console.log(accessToken);
 
-    // You can perform additional actions here, such as verifying the user or saving the user to a database
-
-    // Call the done() callback with the user object
-    done(null, accessToken);
+    done(null, user);
   }
 );
