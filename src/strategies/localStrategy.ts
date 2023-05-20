54276eda5c81f5633381948cd18d5126862d4f25
Gin <ginderick@gmail.com>
2023-05-20 10:44:37 +0800

    refactor localStrategy to be an exported variable

--- src/strategies/localStrategy.ts
+++ src/strategies/localStrategy.ts
@@ -1,20 +1,17 @@
 import passport from 'passport';
 import {Strategy as LocalStrategy} from 'passport-local';
-import prisma from 'prisma';
+import prisma from '../../prisma';
 import bcrypt from 'bcrypt';
 
-passport.use(
-  'local',
-  new LocalStrategy(async function verify(username, password, done) {
-    try {
-      const user: any = prisma.user.findFirst({where: {username: username}});
-      const result = await bcrypt.compare(password, user.hashed_password);
-      if (!user || !result) {
-        return done(null, false, {message: 'Invalid email or password'});
-      }
-      return done(null, user);
-    } catch (error) {
-      return done(error);
+export const localStrategy = new LocalStrategy(async function verify(username, password, done) {
+  try {
+    const user = await prisma.user.findUnique({where: {username: username}});
+    const result = await bcrypt.compare(password, user!.hashed_password);
+    if (!user || !result) {
+      return done(null, false, {message: 'Invalid email or password'});
     }
-  })
-);
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
+});
